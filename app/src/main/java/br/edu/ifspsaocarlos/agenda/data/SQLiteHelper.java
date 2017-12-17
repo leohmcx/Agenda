db7ddcd297a2719b09693ad2bc294c10419059b7
leohcmx <leonardo.henrique45@gmail.com>
2017-12-17 11:01:27 -0200

    Versão 4.0 do app + Controle de Versão

--- app/src/main/java/br/edu/ifspsaocarlos/agenda/data/SQLiteHelper.java
+++ app/src/main/java/br/edu/ifspsaocarlos/agenda/data/SQLiteHelper.java
@@ -2,6 +2,7 @@ package br.edu.ifspsaocarlos.agenda.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 class SQLiteHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "agenda.db";
@@ -14,16 +15,16 @@ class SQLiteHelper extends SQLiteOpenHelper {
     static final String KEY_CELULAR = "celular";
     static final String KEY_DIAANIVERSARIO = "dia";
     static final String KEY_MESANIVERSARIO = "mes";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 4;
     private static final String DATABASE_CREATE = "CREATE TABLE "+ DATABASE_TABLE +" (" +
             KEY_ID  +  " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             KEY_NAME + " TEXT NOT NULL, " +
             KEY_FONE + " TEXT, "  +
-            KEY_EMAIL + " TEXT, " +
-            KEY_FAVORITE + " INTEGER," +
-            KEY_CELULAR + " TEXT," +
-            KEY_DIAANIVERSARIO + " INTEGER," +
-            KEY_MESANIVERSARIO + " INTEGER);";
+            KEY_EMAIL + " TEXT); "; //+
+            //KEY_FAVORITE + " INTEGER," +
+            //KEY_CELULAR + " TEXT," +
+            //KEY_DIAANIVERSARIO + " INTEGER," +
+            //KEY_MESANIVERSARIO + " INTEGER);";
 
 
     SQLiteHelper(Context context) {
@@ -36,7 +37,23 @@ class SQLiteHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase database, int oldVersion, int    newVersion) {
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+
+        Log.e("DataBaseUpgrade", "Updating table from " + oldVersion + " to " + newVersion);
+
+        if (oldVersion < 2){
+            database.execSQL("ALTER TABLE " + DATABASE_TABLE  + " ADD COLUMN "+ KEY_FAVORITE +" INTEGER;");
+        }
+
+        if (oldVersion < 3){
+            database.execSQL("ALTER TABLE " + DATABASE_TABLE  + " ADD COLUMN "+ KEY_CELULAR +" INTEGER;");
+        }
+
+        if (oldVersion <= 4){
+            database.execSQL("ALTER TABLE " + DATABASE_TABLE  + " ADD COLUMN "+ KEY_DIAANIVERSARIO+" INTEGER;");
+            database.execSQL("ALTER TABLE " + DATABASE_TABLE  + " ADD COLUMN "+ KEY_MESANIVERSARIO +" INTEGER;");
+        }
+
     }
 }
 
